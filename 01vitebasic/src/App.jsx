f6b00eb996932cdaab3b48e9d9e3c971cf0a2a88
mrTaushifRaja <taushifraja933@gmail.com>
2025-04-13 16:37:41 +0530

    x

---
+++ 01vitebasic/src/App.jsx
@@ -0,0 +1,11 @@
+import chai from "./chai"
+
+function App() {
+
+
+  return (
+   <h1>hello chai aur code</h1>
+  )
+}
+
+export default App
---
+++ 01vitebasic/src/chai.jsx
@@ -0,0 +1,13 @@
+import chia from "./chai"
+
+function App(){
+  return (
+    <div>
+      <chai/>
+    <h3>chia ids ready from vite react</h3>
+    <p>test chai </p>
+    </div>
+  )
+}
+
+export default App
